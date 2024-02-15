6f7d50bb48b3175561d87e8c7183177d265cedf4
ArpitaPanda261 <arpitapanda261@gmail.com>
2024-02-16 01:02:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 0, lng: 0 },
+      zoom: 2,
+    });
+  
+    // Add markers for destinations
+    const destinations = [
+      { lat: 40.7128, lng: -74.006, name: 'Destination 1' },
+      // Add more destinations with latitudes and longitudes
+    ];
+  
+    destinations.forEach((destination) => {
+      new google.maps.Marker({
+        position: destination,
+        map,
+        title: destination.name,
+      });
+    });
+  }*/
+  
